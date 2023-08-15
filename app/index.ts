01658b48165176cb610df67d6b4fbdae804f50af
Sina Sharifzade <sharifzadesina@gmail.com>
2023-08-15 07:04:40 +0330

    Enable Monero

--- app/index.ts
+++ app/index.ts
@@ -2,8 +2,20 @@ import 'dotenv/config';
 import dataSource from '@app/data-source.js';
 import program from '@app/cli.js';
 import redis from '@app/redis.js';
+import process from 'process';
+import logger from '@app/logger.js';
 
 await redis.connect();
 await dataSource.initialize();
+
+process.on('uncaughtException', (err, origin) => {
+  logger.error(err, err.message);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(reason, 'Unhandled Rejection.', promise);
+  process.exit(1);
+});
 
 await program.parseAsync(process.argv);

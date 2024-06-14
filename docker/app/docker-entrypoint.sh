1768683feebc8cb9ac04221814995d71013b45c5
Sina Sharifzade <sharifzadesina@gmail.com>
2024-06-14 13:19:08 +0330

    Use healthcheck

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env sh
 set -e
 
+if [[ "$1" != "start" ]]; then
+  exec "$@"
+fi
+
 npm --version
 npm install --dev --no-progress --no-audit --no-fund
 
 npm run clear
 npm run build
-
-if [[ "$1" != "start" ]]; then
-  exec "$@"
-fi
-
-/usr/local/bin/wait
 
 npx typeorm migration:run -d ./dist/data-source.js
 
